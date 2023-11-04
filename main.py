f594d55b51c62718ba460ac7065bd4877981816e
Jason Godfrey <ja.godfrey.son@gmail.com>
2023-11-04 10:54:43 -0400

    UI trim

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ st.set_page_config(
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-st.title("Get writing feedback!")
-st.caption("A series of chatbots designed to help your writing style flourish ✍️🤖🌸\n\n Select which chatbot you'd like to talk to on the left.")
+# st.title("Get writing feedback!")
+# st.caption("A series of chatbots designed to help your writing style flourish ✍️🤖🌸\n\n Select which chatbot you'd like to talk to on the left.")
 menu_selection = st.sidebar.radio("Select Chatbot:", ("RhetorIQ", "Workshop Partner"))
 
 if menu_selection == "RhetorIQ":
     st.title("📝 RhetorIQ 📝")
+    st.caption("A series of chatbots designed to help your writing style flourish ✍️🤖🌸\n\n Select which chatbot you'd like to talk to on the left.")
     if "rhetoriq_messages" not in st.session_state:
         st.session_state["rhetoriq_messages"] = [{"role": "assistant", "content": "Hi! 👋 I'm going to help you add more rhetorical devices to your writing. Paste in a paragraph or two, and let's get started!"}]
 
@@ -22,7 +23,7 @@ if menu_selection == "RhetorIQ":
 
     if user_input := st.chat_input("Enter your writing sample for RhetorIQ"):
         # Construct the user's input message for RhetorIQ
-        user_message = f"You are the RhetorIQ teacher. \n\nThe user is going to upload a sample of their writing You are going to point out existing rhetorical devices as well as point out options for incorporating new rhetorical devices. You draw on your comprehensive knowledge of overlooked and obscure but powerful rhetorical devices (e.g. YES to: acyrologia, hypotyposis, prosopopoeia NO to: irony, repetition, exclamation) to suggest which rhetorical devices vould be included in their writing.\n\nThe user wants feedback on this piece of writing:\n\n{user_input}"
+        user_message = f"You are the RhetorIQ teacher. \n\nThe user is going to upload a sample of their writing You are going to point out existing rhetorical devices as well as point out options for incorporating new rhetorical devices. You draw on your comprehensive knowledge obscure and powerful rhetorical devices (e.g. YES to: acyrologia, hypotyposis, prosopopoeia NO to: irony, repetition, exclamation, metaphor) to suggest which rhetorical devices vould be included in their writing. If the user inputs something that doesn't seem like a writing sample, gently and kindly prompt them to upload a paragraph or two of writing for you to look at.\n\nThe user wants feedback on this piece of writing:\n\n{user_input}"
 
         st.session_state.rhetoriq_messages.append({"role": "user", "content": user_message})
         st.chat_message("user").write(user_input)
@@ -49,7 +50,7 @@ elif menu_selection == "Workshop Partner":
 
     if user_input := st.chat_input("Enter your writing sample for Workshop Partner"):
         # Construct the user's input message for Workshop Partner
-        user_message = f"You are the best writing workshop partner of all time. You give one piece of constructive advice that always references a specific section of the user writing. On either side of the constructive advice are two compliments about writing that was done particularly well.\n\nUser Input:\n\n{user_input}"
+        user_message = f"You are the best writing workshop partner of all time. You give one piece of constructive advice that always references a specific section of the user writing. On either side of the constructive advice are two compliments about writing that was done particularly well. If the user inputs something that doesn't seem like a writing sample, gently and kindly prompt them to upload a paragraph or two of writing for you to look at.\n\nUser Input:\n\n{user_input}"
 
         st.session_state.workshop_messages.append({"role": "user", "content": user_message})
         st.chat_message("user").write(user_input)
